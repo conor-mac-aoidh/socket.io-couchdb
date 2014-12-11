6b7519690b315cb2e07d3d2ea807e6d014531ee5
Conor Mac Aoidh <conor@macaoidh.ie>
2014-12-11 10:14:41 +0000

    initail commit of working version of couchdb storage backend, though some improvements to be made

---
+++ README.md
@@ -0,0 +1,19 @@
+# socket.io-couchdb
+
+A plugin for socket.io using couchdb as a storage backend. Based on the [socket.io-redis adapter](https://github.com/Automattic/socket.io-redis).
+
+## How to use
+
+```js
+var io = require('socket.io')(3000);
+var couchdb = require('socket.io-couchdb');
+io.adapter(couchdb({ host: 'localhost', port: 5984, db : 'socket.io' }));
+```
+
+By running socket.io with the `socket.io-couchdb` adapter you can run
+multiple socket.io instances in different processes or servers that can
+all broadcast and emit events to and from each other.
+
+If you need to emit events to socket.io instances from a non-socket.io
+process, you should use [socket.io-couchdb-emitter](http:///github.com/conor-mac-aoidh/socket.io-couchdb-emitter).
+
---
+++ index.js
@@ -0,0 +1,142 @@
+/**
+ * Module dependencies.
+ */
+'use strict';
+
+var uid2 = require('uid2');
+var msgpack = require('msgpack-js');
+var Adapter = require('socket.io-adapter');
+var Emitter = require('events').EventEmitter;
+var debug = require('debug')('socket.io-couchdb');
+var request = require('request');
+var JSONStream = require('JSONStream');
+var domain = require('domain');
+var http = require('http');
+//var nock = require('nock');
+var stream = require('stream');
+var es = require('event-stream');
+//nock.recorder.rec();
+
+/**
+ * Returns a redis Adapter class.
+ *
+ * @param {String} optional, redis uri
+ * @return {RedisAdapter} adapter
+ * @api public
+ */
+
+function adapter(uri, opts){
+  opts = opts || {};
+
+  // handle options only
+  if ('object' == typeof uri) {
+    opts = uri;
+    uri = null;
+  }
+
+  // handle uri string
+  if (uri && !opts.port) {
+    uri = uri.split(':');
+    opts.host = uri[0];
+    opts.port = uri[1];
+  }
+
+  // opts
+  var host = opts.host || '127.0.0.1';
+  var db = opts.db;
+  var port = Number(opts.port || 5984);
+  var prefix = opts.key || 'socket.io';
+  var base = host + ':' + port + '/' + db + '/';
+
+  // this server's key
+  var uid = uid2(6);
+  var key = prefix + '#' + uid;
+
+  /**
+   * Adapter constructor.
+   *
+   * @param {String} namespace name
+   * @api public
+   */
+
+  function CouchDB(nsp){
+    Adapter.call(this, nsp);
+
+    var self = this;
+
+    // setup domain/error catcher
+    // for changes listener
+    var app = domain.create();
+    app.on('error', function(err){
+      debug(err);
+      this.requestChanges();
+    }.bind(this));
+    app.run(function(){
+      this.requestChanges();
+    }.bind(this));
+  }
+  
+  CouchDB.prototype.__proto__ = Adapter.prototype;
+
+  CouchDB.prototype.requestChanges = function(){
+    http.get(base + '/_changes?feed=continuous&heartbeat=1000&include_docs=true&since=now', function(feed){
+      feed
+        .pipe(JSONStream.parse())
+        .pipe(new stream.PassThrough({
+          objectMode : true
+        }))
+        .pipe(es.map(this.onmessage.bind(this)));
+    }.bind(this));
+  };
+
+  CouchDB.prototype.onmessage = function(data, cb){ 
+    if(!data.doc || !data.doc.channel){
+      return;
+    }
+    var pieces = data.doc.channel.split('#');
+    if(uid === pieces.pop()){
+      return debug('ignore same uid');
+    }
+    var args = msgpack.decode(new Buffer(data.doc.msg, 'hex'));
+    debug('message received: ', args);
+
+    if(args[0] && args[0].nsp === undefined){
+      debug('arg namespace is undefined');
+      args[0].nsp = '/';
+    }
+
+    if(!args[0] || args[0].nsp !== this.nsp.name){
+      return debug('ignore different namespace: ' + args[0].nsp + ' != ' + this.nsp.name);
+    }
+
+    args.push(true);
+
+    this.broadcast.apply(this, args);
+
+    cb();
+  };
+
+  CouchDB.prototype.broadcast = function(packet, opts, remote){
+    debug('broadcasting data');
+    Adapter.prototype.broadcast.call(this, packet, opts);
+    // add the message to the db
+    if(!remote){
+      debug('broadcasting message: ', [packet, opts]);
+      var msg = msgpack.encode([packet, opts]).toString('hex');
+//      var msg = [packet, opts];
+      request({
+        url     : base,
+        method  : 'POST',
+        body    :  {
+          channel   : key,
+          msg       : msg
+        },
+        json    : true
+      });
+    }
+  };
+
+  return CouchDB;
+}
+
+module.exports = adapter;
